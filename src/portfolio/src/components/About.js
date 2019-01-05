58e4441050e94f92ea3730c99e43c51bf08393d8
Kenichi Ukai <[ken151102@gmail.com]>
2019-01-05 16:34:09 +0900

    Add local-feature

--- src/portfolio/src/components/About.js
+++ src/portfolio/src/components/About.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
+import i18next from 'i18next';
+import enLocalesTranslationJson from '../locales/en/translation'
+import jaLocalesTranslationJson from '../locales/ja/translation'
+
+i18next
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    resources: {
+      en: {
+        translation: enLocalesTranslationJson
+      },
+      ja: {
+        translation: jaLocalesTranslationJson
+      }
+    }
+  });
+
 class About extends Component {  
+
+  state = {
+  }
   render() {
     return (
       <div>
         <h1>About</h1>
-        <p>This page is ...</p>
+        <h3>{i18next.t('greeting')}</h3>
+        <p>{i18next.t('title')}</p>
       </div>
       )
   }
