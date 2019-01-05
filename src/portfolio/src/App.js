1bf63ae27306cdd3ea1745f41b078e596a047d85
Kenichi Ukai <[ken151102@gmail.com]>
2019-01-05 15:45:16 +0900

    Update some files/folders

--- src/portfolio/src/App.js
+++ src/portfolio/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import About from './components/About';
-import Dashboard from './components/Dashboard';
+import Work from './components/Work';
+import Comment from './components/Comment';
 import { Route, Link, Switch } from 'react-router-dom'; // react-route
 
 
@@ -12,11 +13,13 @@ class App extends Component {
         <div>
           <ul>
             <li><Link to='/about'>About</Link></li>
-            <li><Link to='/dashboard'>Dashboard</Link></li>
+            <li><Link to='/work'>Work</Link></li>
+            <li><Link to='/comment'>Comment</Link></li>
           </ul>
           <hr />
           <Route path='/about' component={About} />
-          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/work' component={Work} />
+          <Route path='/comment' component={Comment} />
         </div>
       </Switch>
     )
--- src/portfolio/src/components/Work.js
+++ src/portfolio/src/components/Work.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class Dashboard extends Component {  
+class Work extends Component {  
   render() {
     return (
     <div>
-      <h1>Building List</h1>
+      <h1>My Work</h1>
     </div>
     )
   }
 }
 
-export default Dashboard;
+export default Work;
